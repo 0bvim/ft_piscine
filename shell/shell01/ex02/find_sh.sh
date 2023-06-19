146ca6a50df36b1b85cc56fb5a26481d485e8477
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-19 09:26:31 -0300

    rtfm

---
+++ shell/shell01/ex02/find_sh.sh
@@ -0,0 +1,3 @@
+basename -s ".sh" $(find . -name "*.sh")
+#use command bellow if you're in a large network, with many drivers and want to search only local.
+#basename -s ".sh" $(find -mount -name "*.sh")
