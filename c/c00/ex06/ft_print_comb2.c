224bc272bc20145bfc8618e2747ecc21da5440f1
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-21 00:53:59 -0300

    combomb2

---
+++ c/c00/ex06/ft_print_comb2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 00:17:37 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/21 00:51:33 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_split_nbr(int a, int b)
+{
+	ft_putchar((a / 10) + '0');
+	ft_putchar((a % 10) + '0');
+		write(1, " ", 1);
+	ft_putchar((b / 10) + '0');
+	ft_putchar((b % 10) + '0');
+	if (a != 98)
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb2(void)
+{
+	int	nb1;
+	int	nb2;
+
+	nb1 = 00;
+	while (nb1 <= 98)
+	{
+		nb2 = nb1 + 1;
+		while (nb2 <= 99)
+		{
+			ft_split_nbr(nb1, nb2);
+			nb2++;
+		}
+		nb1++;
+	}
+}
--- c/c00/ex06/main.c
+++ c/c00/ex06/main.c
@@ -1,7 +1,10 @@
 #include <unistd.h>
 
+void	ft_print_comb2(void);
+
 int	main(void)
 {
 
+	ft_print_comb2();
 	return (0);
 }
