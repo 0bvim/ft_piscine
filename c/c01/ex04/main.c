1d5eea779f24a7644748ad1e9e0fe3b0644519f8
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-23 00:17:35 -0300

    ultimate

--- c/c01/ex04/main.c
+++ c/c01/ex04/main.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b);
+
 int	main(void)
 {
-	
+	int a = 10;
+	int b = 5;
+
+	ft_ultimate_div_mod(&a, &b);
+	printf("%i, %i\n", a, b);
 	return (0);
 }
