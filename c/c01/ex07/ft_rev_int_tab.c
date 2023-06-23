c9ea74d1b9052fb684b19f1e4ba03548a39ab2db
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-23 01:07:33 -0300

    rev_int

---
+++ c/c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 00:32:22 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/23 01:03:51 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int pos;
+	int swap;
+
+	pos = 0;
+	while (pos < (size / 2))
+	{
+		swap = tab[pos];
+		tab[pos] = tab[size - 1 - pos];
+		tab[size - 1 - pos] = swap;
+		pos++;
+	}
+}
--- c/c01/ex07/main.c
+++ c/c01/ex07/main.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size);
+
 int	main(void)
 {
+	int tab[] = {1, 2, 3, 4, 5};
+
+	ft_rev_int_tab(tab, 5);
+
+	printf("%i, %i, %i, %i, %i\n", tab[0], tab[1], tab[2], tab[3], tab[4]);
 	
 	return (0);
 }
