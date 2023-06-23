f571dbdd32fe2fb5e622c64dd1bece0223a9b1e5
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-23 00:10:43 -0300

    divmod

--- c/c01/ex03/main.c
+++ c/c01/ex03/main.c
@@ -1,5 +1,19 @@
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod);
+
 int	main(void)
 {
+	int a = 10;
+	int b = 2;
+	int div;
+	int mod;
+
+	div = 0;
+	mod = 0;
 	
+	ft_div_mod(a, b, &div, &mod);
+
+	printf("%i, %i\n", div, mod);
 	return (0);
 }
