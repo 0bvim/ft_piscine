a966a664c4fa76e1a09c9e6bf99c4eb7a78b7824
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-25 22:12:56 -0300

    lowercase

---
+++ c/c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 22:06:45 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/25 22:12:32 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_lowercase(char *str)
+{
+	while (*str)
+	{
+		if (!(*str >= 97 && *str <= 122))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+#include <stdio.h>
+int    main()
+{
+    printf("%i\n", ft_str_is_lowercase("hello"));
+    printf("%i\n", ft_str_is_lowercase("Hello"));
+    printf("%i\n", ft_str_is_lowercase("i@ello"));
+    printf("%i\n", ft_str_is_lowercase("ello"));
+    printf("%i\n", ft_str_is_lowercase(""));
+    printf("%i\n", ft_str_is_lowercase("  \n\t"));
+}
