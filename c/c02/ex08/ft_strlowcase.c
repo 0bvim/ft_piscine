165f12a65f906c285181c77b17645ce440575e85
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-26 00:15:32 -0300

    ex08

---
+++ c/c02/ex08/ft_strlowcase.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 23:25:33 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/25 23:27:33 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	while (*str)
+	{
+		if (*str >= 65 && *str <= 90)
+		{
+			*str += 32;
+		}
+		str++;
+	}
+	return (str);
+}
+#include <stdio.h>
+
+int main(void)
+{
+  char test[] = "OI TUDO BOM?";
+  
+  ft_strlowcase(test);
+  printf("%s", test);
+}
---
+++ c/c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 23:31:03 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/26 00:14:50 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	while (*str)
+	{
+		if (*str >= 65 && *str <= 90)
+		{
+			*str += 32;
+		}
+		str++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	ft_strlowcase(str);
+	*str -= 32;
+
+	while (*str)
+	{
+		if ((*(str - 1) >= 32 && *(str - 1) <= 47) ||
+			(*(str - 1) >= 58 && *(str - 1) <= 64))
+		{
+			*str -= 32;
+		}
+		str++;
+	}
+	return (str);
+}
+#include <stdio.h>
+
+int main(void)
+{
+	char test[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	ft_strcapitalize(test);
+	printf("%s\n", test);
+}
