8abb0f9c55fba3859c3c3159cb82e02d8ad7f9e0
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-27 01:11:32 -0300

    watch ocean video to more info

---
+++ c/c02/ex10/ft_strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/27 00:45:51 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/27 01:11:00 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	int len;
+
+	len = 0;
+	while (*(src + len) && --size)
+	{
+		*dest++ = *(src + len++);
+	}
+	*dest = '\0';
+	while (*(src + len))
+	{
+		++len;
+	}
+	return(len);
+}
