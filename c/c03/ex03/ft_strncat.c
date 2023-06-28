20c48f0a2674d2870f01f90cd3902362f7c9093e
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-28 00:09:09 -0300

    fixandRTFM

--- c/c03/ex03/ft_strncat.c
+++ c/c03/ex03/ft_strncat.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/27 22:28:26 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/06/27 23:40:41 by vde-frei         ###   ########.fr       */
+/*   Created: 2023/06/27 23:40:25 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/28 00:08:52 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src, unsigned int nb)
 {
+	int i;
+
+	i = 0;
 	while (*dest)
-	{
 		dest++;
-	}
-	while (*dest++ = *src++);
-	return (--dest);
+	while ((*dest++ = *src++) && (nb - 1 > i++));
+	return (dest);
 }
