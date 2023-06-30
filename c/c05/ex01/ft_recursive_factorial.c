c6b8d2d2e84624c5670db485ec16abba4eed3d8c
Vinicius Pereira <niviciusdev@gmail.com>
2023-06-30 01:04:54 -0300

    recursive

--- c/c05/ex01/ft_recursive_factorial.c
+++ c/c05/ex01/ft_recursive_factorial.c
@@ -6,12 +6,14 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 21:23:10 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/06/14 21:30:59 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/06/30 01:04:30 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
+	if (nb < 0)
+		return (0);
 	if (nb == 0 || nb == 1)
 	{
 		return (1);
@@ -21,6 +23,6 @@ int	ft_recursive_factorial(int nb)
 //#include <stdio.h>
 //int main(void)
 //{
-//	printf("%d\n", ft_recursive_factorial(3));
+//	printf("%d\n", ft_recursive_factorial(12));
 //	return 0;
 //}
