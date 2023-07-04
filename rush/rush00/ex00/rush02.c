2ed6d9265921d6d58b05d5b111052d24c3867476
Vinicius Pereira <niviciusdev@gmail.com>
2023-07-04 10:47:14 -0300

    rush 02 don't need to fix

--- rush/rush00/ex00/rush02.c
+++ rush/rush00/ex00/rush02.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush01.c                                           :+:      :+:    :+:   */
+/*   rush02.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 00:42:07 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/04 10:08:52 by vde-frei         ###   ########.fr       */
+/*   Created: 2023/07/04 09:45:41 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/07/04 10:45:32 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,21 @@ void	rush(int x, int y)
 
 	wid = x;
 	len = y;
-	if (x < 0 || y < 0)
+	if (wid < 0 || len < 0)
 		return ;
 	while (--len >= 0)
 	{
 		while (--wid >= 0)
 		{
-			if ((wid == x - 1 && len == y - 1) || (len == 0 && wid == 0))
-				ft_putchar('/');
-			else if ((len == 0 && wid == x - 1) || (wid == 0 && len == y - 1))
-				ft_putchar('\\');
-			else if ((wid < x - 1 && wid > 0) && (!(len == 0 || len == y - 1)))
+			if (((wid == x - 1) || (wid == 0)) && (len == y - 1))
+				ft_putchar('A');
+			else if (((wid == x - 1) || (wid == 0)) && (len == 0))
+				ft_putchar('C');
+			else if ((wid < x - 1 && len < y - 1) && (wid > 0 && len > 0))
 				ft_putchar(' ');
-			else if (!((wid == x - 1 && len == y - 1) || (len == 0 && wid == 0))
-				|| (!((len == 0 && wid == x - 1)
-						|| (len == 0 && wid == 0)) && (wid < x -1 && wid > 0)))
-				ft_putchar('*');
+			else if ((wid < x - 1 && (len == y - 1 || len == 0))
+				|| (!((wid == x - 1 || wid == 0) && (len == y - 1 || len == 0))))
+				ft_putchar('B');
 		}
 		wid = x;
 		ft_putchar('\n');
