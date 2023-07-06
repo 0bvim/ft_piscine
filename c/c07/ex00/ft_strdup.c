37f9379b1640fadfc322e07a218937fb1a26e92e
Vinicius Pereira <niviciusdev@gmail.com>
2023-07-06 01:22:27 -0300

    strdup, nice ex. I learned so much

---
+++ c/c07/ex00/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/06 00:35:57 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/07/06 01:21:39 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	int	len;
+	char	*str;
+
+	len = 0;
+	while (src[len])
+		len++;
+	str = (char *)malloc(sizeof(*src) * (len + 1));
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
